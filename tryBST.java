989735e43b8cf5f3745a4ccdca960846a2d2bab8
hond64 <wentzelmiguel@gmail.com>
2026-03-23 12:24:46 +0000

    Implement BST class with insert method in tryBST.java

--- tryBST.java
+++ tryBST.java
@@ -13,3 +13,18 @@ class Node {
         left = right = null;
     }
 }
+class BST{
+    Node root;
+    public Node insert(Node root, int data){
+        if(root == null){
+            root = new Node(data);
+            return root;
+        }
+        if(data < root.data){
+            root.left = insert(root.left, data);
+        } else if(data > root.data){
+            root.right = insert(root.right, data);
+        }
+        return root;
+    }
+}
